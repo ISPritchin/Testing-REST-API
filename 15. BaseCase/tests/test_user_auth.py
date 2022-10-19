e65ee57358a6f245294b7a9b72fcd3d9785ecc23
ISPritchin <ISPritchin@gmail.com>
2022-10-19 23:07:55 +0300

    bugfix

--- 15. BaseCase/tests/test_user_auth.py
+++ 15. BaseCase/tests/test_user_auth.py
@@ -15,7 +15,7 @@ class TestUserAuth(BaseCase):
 
         self.auth_sid = self.get_cookie(response1, "auth_sid")
         self.token = self.get_header(response1, "x-csrf-token")
-        self.user_id_from_auth_method = self.get_json(response1, "user_id")
+        self.user_id_from_auth_method = self.get_json_value(response1, "user_id")
 
     def test_auth_user_positive(self):
         response = requests.get(
