c46eb33ed45cfabde16a96abe76c1d88b9e81f5c
ISPritchin <ISPritchin@gmail.com>
2022-10-19 23:46:18 +0300

    add folders

--- 02. Передача параметров запроса.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-if __name__ == '__main__':
-    PAYLOAD = {
-        "name": "user"
-    }
-
-    response = requests.get("https://playground.learnqa.ru/api/hello", params=PAYLOAD)
-    print(response.text)
--- 07. Заголовки.py
+++
@@ -1,8 +0,0 @@
-import requests
-from requests import JSONDecodeError
-
-if __name__ == '__main__':
-    response = requests.get("https://playground.learnqa.ru/api/show_all_headers")
-
-    print(response.text)    # Заголовки запроса
-    print(response.headers) # Заголовки ответа
